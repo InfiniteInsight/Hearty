afc1bf065d96465147a3f0144f65d5b2f6ace37b
Evan Nevermore <InfiniteInsight@gmail.com>
2026-05-07 14:32:11 -0400

    feat: BootReceiver + MainActivity FlutterEngine wiring for wake word service

--- hearty_app/android/app/src/main/kotlin/com/hearty/app/MainActivity.kt
+++ hearty_app/android/app/src/main/kotlin/com/hearty/app/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.hearty.app
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // Give the wake word service access to the Flutter binary messenger
+        // so it can call `wakeWordDetected` into Dart.
+        HeartyWakeWordService.flutterBinaryMessenger = flutterEngine.dartExecutor.binaryMessenger
+        val serviceIntent = Intent(this, HeartyWakeWordService::class.java)
+        startForegroundService(serviceIntent)
+    }
+}
