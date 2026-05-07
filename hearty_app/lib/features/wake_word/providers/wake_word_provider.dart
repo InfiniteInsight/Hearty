242840751c8e775fa2bd40526d039b06b7c10c44
Evan Nevermore <InfiniteInsight@gmail.com>
2026-05-07 14:33:47 -0400

    feat: WakeWordChannel + WakeWordProvider — Flutter side of wake word detection

---
+++ hearty_app/lib/features/wake_word/providers/wake_word_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../wake_word_channel.dart';
+
+final wakeWordDetectedProvider =
+    StateNotifierProvider<WakeWordNotifier, bool>((ref) {
+  return WakeWordNotifier()..init();
+});
+
+class WakeWordNotifier extends StateNotifier<bool> {
+  WakeWordNotifier() : super(false);
+
+  void init() {
+    WakeWordChannel.onWakeWordDetected(() => setDetected(true));
+  }
+
+  void setDetected(bool value) => state = value;
+}
